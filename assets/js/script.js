6c11aaa703c7d0499b955a60fe3f7563db6dd372
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-04-28 18:04:22 +0200

    Ajout de l'eventListener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,12 @@
 // Répertorier tous les enfants de l'ul
-const ul = document.querySelector('ul');
+const ul = document.querySelector("ul");
+
 // Récupère tous les enfants
 const children = ul.childNodes;
+
 // Boucle sur les enfants de l'ul
 for (let i = 0; i < children.length; i++) {
-    // Vérifie si le noeud enfant est bien un noeud élément
+  // Vérifie si le noeud enfant est bien un noeud élément
   if (children[i].nodeType === 1) {
     console.log(children[i]);
   }
@@ -12,12 +14,21 @@ for (let i = 0; i < children.length; i++) {
 
 // Vérifier si l'élément "Fast and Furious" est le premier élément
 const firstChild = ul.firstElementChild;
-if(firstChild.textContent === "Fast and Furious") {
-    // Si ce n'est pas le cas, insertBefore pour le mettre
-    ul.insertBefore(firstChild, ul.firstElementChild);
-    console.log(firstChild);
+if (firstChild.textContent === "Fast and Furious") {
+  // Si ce n'est pas le cas, insertBefore pour le mettre
+  ul.insertBefore(firstChild, ul.firstElementChild);
+  console.log(firstChild);
 }
 
 // Ajouter la classe .important à "Fast and Furious"
-const fastAndFurious = document.querySelector('li:first-of-type');
+const fastAndFurious = document.querySelector("li:first-of-type");
 fastAndFurious.classList.add("important");
+
+// Ajouter un eventListener sur chaque élément de la liste
+// et ajouter une alerte pour affficher l'élément cliqué
+const childrenClick = document.querySelectorAll("li");
+childrenClick.forEach(function (element) {
+  element.addEventListener("click", function () {
+    alert(element.textContent);
+  });
+});
